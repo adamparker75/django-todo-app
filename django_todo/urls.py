22266181b0e5e10f07446991103781832f22cfee
adamparker75 <adamparker75@sky.com>
2020-12-16 13:46:59 +0000

    Added delete and toggle functionality

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -12,13 +12,20 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+This angular bracket syntax here is common in Django URLs.
+And is the mechanism by which the item ID makes its way from links or forms
+in our templates.
+Through the URL and into the view which expects it as a parameter.
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import get_todo_list, add_item
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', get_todo_list, name='get_todo_list'),
-    path('add', add_item, name='add')
+    path('', views.get_todo_list, name='get_todo_list'),
+    path('add', views.add_item, name='add'),
+    path('edit/<item_id>', views.edit_item, name='edit'),
+    path('toggle/<item_id>', views.toggle_item, name='toggle'),
+    path('delete/<item_id>', views.delete_item, name='delete')
 ]
